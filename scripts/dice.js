43632c9ce171ccebb92f411d3ddfb41c8e719b05
Chirag U <1078618+chiragu@users.noreply.github.com>
2020-08-12 12:05:01 -0400

    Fixed bug in invention dice

--- scripts/dice.js
+++ scripts/dice.js
@@ -9,8 +9,8 @@ function setDice() {
     for (let i=0; i<dice.length; i++) {
 
         let dice1roll = Math.floor(Math.random() * 6) + 1;
-        dice[i].innerHTML = "<img src='images/dice/dice" + (i+1) + "_" + dice1roll +  ".png' alt='dice'>";
-        console.log("<img src='images/dice/dice" + (i+1) + "_" + dice1roll +  ".png' alt='dice'>");
+        dice[i].innerHTML = "<img src='images/dice/dice" + dice1roll + "_" +  (i+1)+  ".png' alt='dice'>";
+        //console.log("<img src='images/dice/dice" + dice1roll + "_" +  (i+1)+  ".png' alt='dice'>");
 
     }
 }
