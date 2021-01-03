5df14c91f277087f1ee9b40df4fea02e64808856
Chirag U <1078618+chiragu@users.noreply.github.com>
2021-01-02 20:23:27 -0500

    email js to vanilla from jquery

--- scripts/emailLambdaAPI.js
+++ scripts/emailLambdaAPI.js
@@ -1,23 +1,30 @@
 function submitToAPI(e) {
+
+    // prevent default redirect
     e.preventDefault();
+
+    // REST Endpoint
     var URL = "https://1yppsnty00.execute-api.us-east-1.amazonaws.com/Prod/contact-us";
 
-         var Namere = /[A-Za-z]{1}[A-Za-z]/;
-         if (!Namere.test($("#name").val())) {
-                      alert ("Name can not less than 2 char");
-             return;
-         }
-         if ($("#email").val()=="") {
-             alert ("Please enter your email");
-             return;
-         }
+    
+    // Input validation
+    var Namere = /[A-Za-z]{1}[A-Za-z]/;
+    if (!Namere.test($("#name").val())) {
+                alert ("Name can not less than 2 characters");
+        return;
+    }
+    if ($("#email").val()=="") {
+        alert ("Please enter your email");
+        return;
+    }
 
-         var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-         if (!reeamil.test($("#email").val())) {
-             alert ("Please enter valid email address");
-             return;
-         }
+    var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+    if (!reeamil.test($("#email").val())) {
+        alert ("Please enter valid email address");
+        return;
+    }
 
+    // Create JSON Object with data from contact form
     var name = $("#name").val();
     var email = $("#email").val();
     var message = $("#message").val();
@@ -27,24 +34,36 @@ function submitToAPI(e) {
        message : message
      };
 
-    $.ajax({
-      type: "POST",
-      url : "https://1yppsnty00.execute-api.us-east-1.amazonaws.com/Prod/contact-us",
-      dataType: "json",
-      crossDomain: "true",
-      contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(data),
 
-      
-      success: function () {
+    // Send data to REST Endpoint to Email
+
+    // configure a request
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', URL);
+
+    // set headers
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+    // send request
+    xhr.send(data);
+
+    // listen for `load` event
+    xhr.onload = () => {
+        
         // clear form and show a success message
+        console.log(xhr.responseText);
         alert("Your message has been sent. Thank you!");
         document.getElementById("contact-form").reset();
-    location.reload();
-      },
-      error: function (xhr, status, error) {
-        // show an error message
-        var errorMessage = xhr.status + ': ' + xhr.statusText;
-        alert("Email Sending Failed\nError Code: " + errorMessage);
-      }});
+        location.reload();
+    }
+
+    // listen for `error` event
+    xhr.onerror = () => {
+        
+         // show an error message
+         var errorMessage = xhr.status + ': ' + xhr.statusText;
+         alert("Email Sending Failed\nError Code: " + errorMessage);
+    }
+
   }
